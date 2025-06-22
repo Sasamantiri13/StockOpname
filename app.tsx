2a83b9b24c750a260bd99d633968a114c3512da6
Sasamantiri13 <sasamantiri2@gmail.com>
2025-06-22 21:38:40 +0700

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockOpnameDSS from './stock-opname-dss';
+
+function App() {
+  return (
+    <div className="App">
+      <StockOpnameDSS />
+    </div>
+  );
+}
+
+export default App;
